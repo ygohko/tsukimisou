3d3176fd3ff9e1753389570218c20ff242ee21fb
Yasuaki Gohko <ygohko@gmail.com>
2022-06-24 13:39:30 +0900

    Divided the main.dart.

--- lib/app.dart
+++ lib/app.dart
@@ -22,8 +22,19 @@
 
 import 'package:flutter/material.dart';
 
-import "app.dart";
+import 'home_page.dart';
 
-void main() {
-  runApp(const App());
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tsukimisou',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
