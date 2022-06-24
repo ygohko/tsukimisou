5f8ee06a88867200c51aef22ceec82ac9de5b4e1
Yasuaki Gohko <ygohko@gmail.com>
2022-06-24 19:10:18 +0900

    Added test codes.

---
+++ lib/memo_store_saver.dart
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2022 Yasuaki Gohko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE ABOVE LISTED COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+import 'memo_store.dart';
+
+class MemoStoreSaver {
+  MemoStore? _memoStore = null;
+  var _fileName = '';
+
+  MemoStoreSaver(MemoStore memoStore, String fileName) {
+    // TODO: Implement this
+    _memoStore = memoStore;
+    _fileName = fileName;
+  }
+
+  void execute() async {
+    // TODO: Implement this
+
+
+
+    final applicationDocumentsDirectory = await getApplicationDocumentsDirectory();
+    var path = applicationDocumentsDirectory.path;
+    print('path: ${path}\n');
+    // final platform = Platform();
+    path = path + Platform.pathSeparator + _fileName;
+    print('path: ${path}\n');
+    final file = File(path);
+
+
+    if (_memoStore == null) {
+        return;
+    }
+    var string = '';
+    final memos = _memoStore!.getMemos();
+    for (final memo in memos) {
+      print('memo: ${memo}\n');
+      string += memo + '\n';
+    }
+    await file.writeAsString(string);
+
+  }
+}
