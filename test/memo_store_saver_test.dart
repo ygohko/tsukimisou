e80c587af09febdd37a93660cfbaaa0bbc55eb15
Yasuaki Gohko <ygohko@gmail.com>
2022-06-26 09:33:24 +0900

    Added tests for MemoStoreSaver.

---
+++ test/memo_store_saver_test.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:tsukimisou/memo_store.dart';
+import 'package:tsukimisou/memo_store_saver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('MemoStoreSaver', () {
+      test('MemoStoreSaver should be created from memo store and path', () {
+          expect(MemoStoreSaver(MemoStore(), './test.json'), isNotNull);
+      });
+
+      test('MemoStoreSaver should save memos as JSON.', () async {
+          final memoStore = MemoStore();
+          memoStore.addMemo('This is a test.');
+          final memoStoreSaver = MemoStoreSaver(memoStore, './test.json');
+          await memoStoreSaver.execute();
+          final file = File('./test.json');
+          final exists = await file.exists();
+          expect(exists, true);
+          final string = await file.readAsString();
+          expect(string, '["This is a test."]');
+          await file.delete();
+      });
+
+      test('MemoStoreSaver.getFromFile() should return memo store saver.', () async {
+          expect(MemoStoreSaver.getFromFileName(MemoStore(), 'test.json'), isNotNull);
+      });
+  });
+}
