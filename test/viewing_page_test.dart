bdfe90388ed34a13bb96ce5ace280008f2fcc730
Yasuaki Gohko <ygohko@gmail.com>
2022-07-02 21:03:22 +0900

    Added tests for ViewingPage.

---
+++ test/viewing_page_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tsukimisou/memo.dart';
+import 'package:tsukimisou/viewing_page.dart';
+
+void main() {
+  testWidgets('ViewingPage widget smoke test', (WidgetTester tester) async {
+      final memo = Memo();
+      memo.text = 'This is a test.';
+      await tester.pumpWidget(MaterialApp(
+        home: ViewingPage(memo: memo),
+      )
+    );
+    expect(find.text('This is a test.'), findsOneWidget);
+  });
+}
