4a7e1c4c521f95df335e6e84bdb626508212bb36
Yasuaki Gohko <ygohko@gmail.com>
2022-07-10 14:42:49 +0900

    Added tests for MemoStoreGoogleDriveLoader.

---
+++ test/memo_store_google_drive_loader_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+import 'package:tsukimisou/memo_store.dart';
+import 'package:tsukimisou/memo_store_google_drive_loader.dart';
+
+void main() {
+  group('MemoStoreGoogleDriveLoader', () {
+    test('MemoStoreGoogleDriveLoader should be created from memo store and file name', () {
+      expect(MemoStoreGoogleDriveLoader(MemoStore(), 'test.json'), isNotNull);
+    });
+  });
+}
