b49acf42c51bc3375f22700517909daf1dfaf2a0
Yasuaki Gohko <ygohko@gmail.com>
2022-07-21 18:01:19 +0900

    Fixed issue where widgett tests are failed.

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tsukimisou/home_page.dart';
 
 void main() {
   testWidgets('HomePage widget smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+      ],
       home: const HomePage(),
     ));
     expect(find.text('Tsukimisou'), findsOneWidget);
