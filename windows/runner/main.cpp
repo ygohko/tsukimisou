3d839a85e15070e2c5be56eaf687df45562d1c56
ygohko <ygohko@gmail.com>
2022-09-08 20:07:18 +0900

    Updated initial windows size for Windows. (#108)

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(640, 720);
+  Win32Window::Size size(880, 720);
   if (!window.CreateAndShow(L"Tsukimisou", origin, size)) {
     return EXIT_FAILURE;
   }
