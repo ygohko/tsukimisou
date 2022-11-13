3fba28ed009a0ac7c6d497588215f4bbce5c728f
Yasuaki Gohko <ygohko@gmail.com>
2022-11-13 12:39:06 +0900

    Fixed tests for HomePage.

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:tsukimisou/home_page.dart';
+import 'package:tsukimisou/memo_store.dart';
 
 void main() {
-  // TODO: Fix test failure.
   testWidgets('HomePage widget smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-      ],
-      home: const HomePage(),
-    ));
+    await tester.pumpWidget(ChangeNotifierProvider(
+        create: (context) => MemoStore(),
+        child: MaterialApp(
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+          ],
+          home: const HomePage(),
+        ),
+      ),
+    );
     expect(find.text('Tsukimisou'), findsOneWidget);
     expect(find.byIcon(Icons.add), findsOneWidget);
     await tester.tap(find.byIcon(Icons.add));
