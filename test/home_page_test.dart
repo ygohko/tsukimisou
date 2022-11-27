f516ee707a48d2fcbf3a75edd7103701f63f7a33
ygohko <ygohko@gmail.com>
2022-11-27 20:02:33 +0900

    Improve test coverage (#156)
    * Updated a test for home page.
    
    * Added tests for vieweing page.
    
    * Updated tests.
    
    * Updated tests.
    
    * Added init().
    
    * Added groups.

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -6,24 +6,37 @@ import 'package:tsukimisou/factories.dart';
 import 'package:tsukimisou/home_page.dart';
 import 'package:tsukimisou/memo_store.dart';
 
+Future<void> init(WidgetTester tester) async {
+  await tester.pumpWidget(
+    ChangeNotifierProvider(
+      create: (context) => MemoStore(),
+      child: MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+        ],
+        home: const HomePage(),
+      ),
+    ),
+  );
+}
+
 void main() {
   Factories.init(FactoriesType.test);
 
-  testWidgets('HomePage widget smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      ChangeNotifierProvider(
-        create: (context) => MemoStore(),
-        child: MaterialApp(
-          localizationsDelegates: [
-            AppLocalizations.delegate,
-          ],
-          home: const HomePage(),
-        ),
-      ),
-    );
-    expect(find.text('Tsukimisou'), findsOneWidget);
-    expect(find.byIcon(Icons.add), findsOneWidget);
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  group('HomePage', () {
+    testWidgets('HomePage shoud have specified widgets.', (WidgetTester tester) async {
+      await init(tester);
+      expect(find.text('Tsukimisou'), findsOneWidget);
+      expect(find.byIcon(Icons.add), findsOneWidget);
+    });
+
+    testWidgets('HomePage shoud show EditingPage when user taps add button.', (WidgetTester tester) async {
+      await init(tester);
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      expect(find.textContaining('Add a new memo'), findsOneWidget);
+      expect(find.byIcon(Icons.done), findsOneWidget);
+      expect(find.byIcon(Icons.close), findsOneWidget);
+    });
   });
 }
--- test/viewing_page_test.dart
+++ test/viewing_page_test.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:tsukimisou/factories.dart';
 import 'package:tsukimisou/memo.dart';
+import 'package:tsukimisou/memo_store.dart';
 import 'package:tsukimisou/viewing_page.dart';
 
+Future<void> init(WidgetTester tester, Memo memo) async {
+  memo.text = 'This is a test.';
+  await tester.pumpWidget(
+    ChangeNotifierProvider(
+      create: (context) => MemoStore(),
+      child: MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+        ],
+        home: ViewingPage(memo: memo),
+      ),
+    ),
+  );;
+}
+
 void main() {
-  testWidgets('ViewingPage widget smoke test', (WidgetTester tester) async {
-    final memo = Memo();
-    memo.text = 'This is a test.';
-    await tester.pumpWidget(MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-      ],
-      home: ViewingPage(memo: memo),
-    ));
-    expect(find.text('This is a test.'), findsOneWidget);
-    expect(find.byIcon(Icons.edit), findsOneWidget);
+  Factories.init(FactoriesType.test);
+
+  group('ViewingPage', () {
+    testWidgets('ViewingPage should have specified widgets.', (WidgetTester tester) async {
+      final memo = Memo();
+      await init(tester, memo);
+      expect(find.byIcon(Icons.close), findsOneWidget);
+      expect(find.byIcon(Icons.share), findsOneWidget);
+      expect(find.byIcon(Icons.delete), findsOneWidget);
+      expect(find.byIcon(Icons.edit), findsOneWidget);
+      expect(find.textContaining('Memo at'), findsOneWidget);
+      expect(find.text('This is a test.'), findsOneWidget);
+      expect(find.textContaining('Updated:'), findsOneWidget);
+      expect(find.textContaining('Tags:'), findsOneWidget);
+    });
+
+    testWidgets('ViewingPage should show confirmation dialog when user taps delete button.', (WidgetTester tester) async {
+      final memo = Memo();
+      await init(tester, memo);
+      await tester.tap(find.byIcon(Icons.delete));
+      await tester.pump();
+      expect(find.text('Confirm'), findsOneWidget);
+    });
+
+    testWidgets('ViewingPage should show EditingPage when user taps edit button.', (WidgetTester tester) async {
+      final memo = Memo();
+      await init(tester, memo);
+      await tester.tap(find.byIcon(Icons.edit));
+      await tester.pump();
+      expect(find.text('Edit a memo'), findsOneWidget);
+    });
+
+    testWidgets('ViewingPage should show EditingPage when user taps edit button.', (WidgetTester tester) async {
+      final memo = Memo();
+      await init(tester, memo);
+      await tester.tap(find.textContaining('Tags:'));
+      await tester.pump();
+      expect(find.text('Bind tags'), findsOneWidget);
+    });
   });
 }
