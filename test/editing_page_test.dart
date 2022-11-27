c124042cfb67f4919f8605f96b315bff306494ea
Yasuaki Gohko <ygohko@gmail.com>
2022-11-27 17:54:49 +0900

    Added init().

--- test/editing_page_test.dart
+++ test/editing_page_test.dart
@@ -5,17 +5,21 @@ import 'package:tsukimisou/editing_page.dart';
 import 'package:tsukimisou/factories.dart';
 import 'package:tsukimisou/memo.dart';
 
+Future<void> init(WidgetTester tester, Memo? memo) async {
+  await tester.pumpWidget(MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+      ],
+      home: EditingPage(memo: memo),
+  ));
+}
+
 void main() {
   Factories.init(FactoriesType.test);
 
   group('Memo', () {
     testWidgets('EditingPage widget smoke test', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
-        localizationsDelegates: [
-          AppLocalizations.delegate,
-        ],
-        home: const EditingPage(),
-      ));
+      await init(tester, null);
       expect(find.text('Add a new memo'), findsOneWidget);
       expect(find.byIcon(Icons.done), findsOneWidget);
     });
@@ -23,12 +27,7 @@ void main() {
     testWidgets('EditingPage widget smoke test', (WidgetTester tester) async {
       final memo = Memo();
       memo.text = 'This is a test.';
-      await tester.pumpWidget(MaterialApp(
-        localizationsDelegates: [
-          AppLocalizations.delegate,
-        ],
-        home: EditingPage(memo: memo),
-      ));
+      await init(tester, memo);
       expect(find.text('Edit a memo'), findsOneWidget);
       expect(find.text('This is a test.'), findsOneWidget);
       expect(find.byIcon(Icons.done), findsOneWidget);
