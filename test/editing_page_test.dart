16361fb3c07bc4c5a388a5796f1f5c0834738fd0
Yasuaki Gohko <ygohko@gmail.com>
2022-12-03 16:39:57 +0900

    Added tests for editing page.

--- test/editing_page_test.dart
+++ test/editing_page_test.dart
@@ -1,36 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:tsukimisou/editing_page.dart';
 import 'package:tsukimisou/factories.dart';
 import 'package:tsukimisou/memo.dart';
+import 'package:tsukimisou/memo_store.dart';
 
 Future<void> init(WidgetTester tester, Memo? memo) async {
-  await tester.pumpWidget(MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-      ],
-      home: EditingPage(memo: memo),
-  ));
+  await tester.pumpWidget(
+    ChangeNotifierProvider(
+      create: (context) => MemoStore(),
+      child: MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+        ],
+        home: EditingPage(memo: memo),
+      ),
+    ),
+  );
 }
 
 void main() {
   Factories.init(FactoriesType.test);
 
   group('EditingPage', () {
-    testWidgets('EditingPage widget smoke test', (WidgetTester tester) async {
+    testWidgets('EditingPage should have specified widgets when adding a new memo.', (WidgetTester tester) async {
       await init(tester, null);
       expect(find.text('Add a new memo'), findsOneWidget);
       expect(find.byIcon(Icons.done), findsOneWidget);
     });
 
-    testWidgets('EditingPage widget smoke test', (WidgetTester tester) async {
+    testWidgets('EditingPage should have specified widgets when editing a memo.', (WidgetTester tester) async {
       final memo = Memo();
       memo.text = 'This is a test.';
       await init(tester, memo);
       expect(find.text('Edit a memo'), findsOneWidget);
       expect(find.text('This is a test.'), findsOneWidget);
       expect(find.byIcon(Icons.done), findsOneWidget);
+    });
+
+    testWidgets('EditingPage should have specified widgets when adding a new memo.', (WidgetTester tester) async {
+      final memo = Memo();
+      await init(tester, memo);
+      await tester.enterText(find.byType(TextField), 'This is a text');
+      await tester.pump();
+      await tester.tap(find.byIcon(Icons.done));
+      await tester.pump();
+      expect(memo.text, 'This is a text');
     });
   });
 }
