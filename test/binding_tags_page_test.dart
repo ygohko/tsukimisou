f6a7fb2bb5ab41ddfb76791f29af8a3223590bae
ygohko <ygohko@gmail.com>
2022-12-03 16:48:49 +0900

    Improve test coverage (#158)
    * Added tests for binding tags page.
    
    * Added tests for editing page.
    
    * Cleaned up.

--- test/binding_tags_page_test.dart
+++ test/binding_tags_page_test.dart
@@ -5,26 +5,54 @@ import 'package:tsukimisou/binding_tags_page.dart';
 import 'package:tsukimisou/factories.dart';
 import 'package:tsukimisou/memo.dart';
 
-void main() {
-  Factories.init(FactoriesType.test);
-
-  testWidgets('BindingTagsPage widget smoke test', (WidgetTester tester) async {
-    final memo = Memo();
-    memo.text = 'This is a test.';
-    memo.tags = ['a', 'b', 'c'];
-    await tester.pumpWidget(MaterialApp(
+Future<void> init(WidgetTester tester, Memo memo) async {
+  await tester.pumpWidget(MaterialApp(
       localizationsDelegates: [
         AppLocalizations.delegate,
       ],
       home: BindingTagsPage(memo: memo, additinalTags: ['d', 'e', 'f']),
-    ));
-    expect(find.text('a'), findsOneWidget);
-    expect(find.text('b'), findsOneWidget);
-    expect(find.text('c'), findsOneWidget);
-    expect(find.text('d'), findsOneWidget);
-    expect(find.text('e'), findsOneWidget);
-    expect(find.text('f'), findsOneWidget);
-    expect(find.byIcon(Icons.check_circle), findsNWidgets(3));
-    expect(find.byIcon(Icons.check_circle_outline), findsNWidgets(3));
-  });
+  ));
+}
+
+void main() {
+  Factories.init(FactoriesType.test);
+
+  group('BindingTagsPage', () {
+    testWidgets('BindingTagsPage should have specified widgets.', (WidgetTester tester) async {
+        final memo = Memo();
+        memo.text = 'This is a test.';
+        memo.tags = ['a', 'b', 'c'];
+        await init(tester, memo);
+        expect(find.text('a'), findsOneWidget);
+        expect(find.text('b'), findsOneWidget);
+        expect(find.text('c'), findsOneWidget);
+        expect(find.text('d'), findsOneWidget);
+        expect(find.text('e'), findsOneWidget);
+        expect(find.text('f'), findsOneWidget);
+        expect(find.byIcon(Icons.check_circle), findsNWidgets(3));
+        expect(find.byIcon(Icons.check_circle_outline), findsNWidgets(3));
+    });
+
+    testWidgets('BindingTagsPage should add tags to memo.', (WidgetTester tester) async {
+        final memo = Memo();
+        memo.text = 'This is a test.';
+        memo.tags = ['a', 'b', 'c'];
+        await init(tester, memo);
+        await tester.tap(find.text('d'));
+        await tester.pump();
+        expect(find.byIcon(Icons.check_circle), findsNWidgets(4));
+        expect(find.byIcon(Icons.check_circle_outline), findsNWidgets(2));
+    });
+
+    testWidgets('BindingTagsPage should remove tags from memo.', (WidgetTester tester) async {
+        final memo = Memo();
+        memo.text = 'This is a test.';
+        memo.tags = ['a', 'b', 'c'];
+        await init(tester, memo);
+        await tester.tap(find.text('a'));
+        await tester.pump();
+        expect(find.byIcon(Icons.check_circle), findsNWidgets(2));
+        expect(find.byIcon(Icons.check_circle_outline), findsNWidgets(4));
+    });
+});
 }
