75fdb4649ea461a627fd5b3c79726e7c7f43dc12
Yasuaki Gohko <ygohko@gmail.com>
2022-12-30 10:41:31 +0900

    Updated tests.

--- test/app_test.dart
+++ test/app_test.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:tsukimisou/app.dart';
+import 'package:tsukimisou/app_state.dart';
 import 'package:tsukimisou/memo_store.dart';
 
 void main() {
   testWidgets('App widget smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(
-      ChangeNotifierProvider(
-          create: (context) => MemoStore(), child: const App()),
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<MemoStore>(create: (context) => MemoStore()),
+          ChangeNotifierProvider<AppState>(create: (context) => AppState()),
+        ],
+        child: const App(),
+      ),
     );
     expect(find.text('Tsukimisou'), findsOneWidget);
     expect(find.byIcon(Icons.add), findsOneWidget);
