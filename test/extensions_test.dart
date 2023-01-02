0ef92241cd870e36d5c37c90679c87e9c2241df2
Yasuaki Gohko <ygohko@gmail.com>
2023-01-02 12:38:58 +0900

    Fixed issue where a test for StringConverter extension fails at Jan 2.

--- test/extensions_test.dart
+++ test/extensions_test.dart
@@ -14,8 +14,13 @@ void main() {
       var dateTime = DateTime(now.year, now.month, now.day, 3, 4);
       expect(dateTime.toSmartString(), '03:04');
 
-      dateTime = DateTime(now.year, 1, 2, 3, 4);
-      expect(dateTime.toSmartString(), '01/02 03:04');
+      if (now.month != 1 && now.day != 2) {
+        dateTime = DateTime(now.year, 1, 2, 3, 4);
+        expect(dateTime.toSmartString(), '01/02 03:04');
+      } else {
+        dateTime = DateTime(now.year, 1, 3, 3, 4);
+        expect(dateTime.toSmartString(), '01/03 03:04');
+      }
 
       dateTime = DateTime(2000, 1, 2, 3, 4);
       expect(dateTime.toSmartString(), '2000/01/02');
