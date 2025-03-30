24c0fe95c5516663e0232912d401a88e928c5414
Yasuaki Gohko <ygohko@gmail.com>
2025-03-30 17:38:56 +0900

    Added a test.

--- test/markdown_parser_test.dart
+++ test/markdown_parser_test.dart
@@ -1,44 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 import 'package:tsukimisou/markdown_parser.dart';
-import 'package:tsukimisou/memo.dart';
-import 'package:tsukimisou/memo_store.dart';
-import 'package:tsukimisou/viewing_page.dart';
+
+Future<void> init(WidgetTester tester) async {
+  await tester.pumpWidget(
+    MaterialApp(
+      home: Text('This is a test.'),
+    ),
+  );
+}
 
 void main() {
   group('MarkdownParser', () {
       testWidgets('MarkdownParser should be created.',
         (WidgetTester tester) async {
-          /*
-          final memo = Memo();
-          memo.text = 'This is a test.';
-          */
-          await tester.pumpWidget(
-            MaterialApp(
-              home: Text('This is a test.'),
-            ),
-            /*
-            ChangeNotifierProvider(
-              create: (context) => MemoStore(),
-              child: MaterialApp(
-                localizationsDelegates: const [
-                  AppLocalizations.delegate,
-                ],
-                home: ViewingPage(memo: memo),
-              ),
-            ),
-            */
-          );
+          await init(tester);
           final context = tester.element(find.text('This is a test.'));
-          /*
-          final context = tester.element(find.byWidgetPredicate(
-              (widget) => widget is RichText && widget.text.toPlainText() == 'This is a test.',
-            )
-          );
-          */
           final parser = MarkdownParser(context, '# Hello, World!');
+      });
+
+      testWidgets('MarkdownParser should be executable.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final parser = MarkdownParser(context, '# Hello, World!');
+          parser.execute();
+      });
+
+      testWidgets('MarkdownParser should create widgets for healines large.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final textTheme = Theme.of(context).textTheme;
+          final parser = MarkdownParser(context, '# Hello, World!');
+          parser.execute();
+          final contents = parser.contents;
+          final column = contents as Column;
+          final widget = column.children[0];
+          final richText = widget as RichText;
+          final span = richText.text as TextSpan;
+          expect(span.style, textTheme.headlineLarge);
+          expect(span.toPlainText(), 'Hello, World!');
       });
   });
 }
