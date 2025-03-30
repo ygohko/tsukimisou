067396a1f361c80282ef12cb71d14a7d199b87f1
Yasuaki Gohko <ygohko@gmail.com>
2025-03-30 18:27:19 +0900

    Generate widgets to display rich texts (#284)
    * Added a test for MarkdownParser.
    
    * Removed unneeded widgets.
    
    * Added a test.
    
    * Added a test.

---
+++ test/markdown_parser_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tsukimisou/markdown_parser.dart';
+
+Future<void> init(WidgetTester tester) async {
+  await tester.pumpWidget(
+    MaterialApp(
+      home: Text('This is a test.'),
+    ),
+  );
+}
+
+void main() {
+  group('MarkdownParser', () {
+      testWidgets('MarkdownParser should be created.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final parser = MarkdownParser(context, '# Hello, World!');
+      });
+
+      testWidgets('MarkdownParser should be executable.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final parser = MarkdownParser(context, '# Hello, World!');
+          parser.execute();
+      });
+
+      testWidgets('MarkdownParser should create widgets for body texts.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final textTheme = Theme.of(context).textTheme;
+          final parser = MarkdownParser(context, 'Hello, World!');
+          parser.execute();
+          final contents = parser.contents;
+          final column = contents as Column;
+          final widget = column.children[0];
+          final richText = widget as RichText;
+          final span = richText.text as TextSpan;
+          expect(span.style, textTheme.bodyMedium);
+          expect(span.toPlainText(), 'Hello, World!');
+      });
+
+      testWidgets('MarkdownParser should create widgets for large healines.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final textTheme = Theme.of(context).textTheme;
+          final parser = MarkdownParser(context, '# Hello, World!');
+          parser.execute();
+          final contents = parser.contents;
+          final column = contents as Column;
+          final widget = column.children[0];
+          final richText = widget as RichText;
+          final span = richText.text as TextSpan;
+          expect(span.style, textTheme.headlineLarge);
+          expect(span.toPlainText(), 'Hello, World!');
+      });
+
+      testWidgets('MarkdownParser should create widgets for medium headlines.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final textTheme = Theme.of(context).textTheme;
+          final parser = MarkdownParser(context, '## Hello, World!');
+          parser.execute();
+          final contents = parser.contents;
+          final column = contents as Column;
+          final widget = column.children[0];
+          final richText = widget as RichText;
+          final span = richText.text as TextSpan;
+          expect(span.style, textTheme.headlineMedium);
+          expect(span.toPlainText(), 'Hello, World!');
+      });
+
+      testWidgets('MarkdownParser should create widgets for small headlines.',
+        (WidgetTester tester) async {
+          await init(tester);
+          final context = tester.element(find.text('This is a test.'));
+          final textTheme = Theme.of(context).textTheme;
+          final parser = MarkdownParser(context, '### Hello, World!');
+          parser.execute();
+          final contents = parser.contents;
+          final column = contents as Column;
+          final widget = column.children[0];
+          final richText = widget as RichText;
+          final span = richText.text as TextSpan;
+          expect(span.style, textTheme.headlineSmall);
+          expect(span.toPlainText(), 'Hello, World!');
+      });
+  });
+}
