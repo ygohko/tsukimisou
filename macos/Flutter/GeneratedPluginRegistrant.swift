c43bb3308a6274ee3062d679a3b1811ee29027c0
Yasuaki Gohko <ygohko@gmail.com>
2025-05-11 09:49:16 +0900

    Updated share_plus to 7.2.1.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import flutter_secure_storage_macos
 import google_sign_in_ios
 import package_info_plus
 import path_provider_foundation
-import share_plus_macos
+import share_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
