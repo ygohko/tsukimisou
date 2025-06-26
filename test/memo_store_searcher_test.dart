7f4de1a19b3d233f2a61863d929f0e1e9878fc41
Yasuaki Gohko <ygohko@gmail.com>
2025-06-27 07:27:05 +0900

    Added a test for MemoStoreSearcher.

--- test/memo_store_searcher_test.dart
+++ test/memo_store_searcher_test.dart
@@ -26,7 +26,7 @@ void main() {
     });
 
     test(
-        'MemoStoreSearcher should find a memo that its text contains keywords.',
+        'MemoStoreSearcher should find a memo that its tags contain keywords.',
         () {
       final memoStore = MemoStore();
       final memo = Memo();
@@ -46,6 +46,27 @@ void main() {
       expect(searcher.results.length, 1);
     });
 
+    test(
+        'MemoStoreSearcher should find a memo that its name contains keywords.',
+        () {
+      final memoStore = MemoStore();
+      final memo = Memo();
+      memo.text = '';
+      memo.name = 'test テスト';
+      memoStore.addMemo(memo);
+      var searcher = MemoStoreSearcher(memoStore, 'test');
+      searcher.execute();
+      expect(searcher.results.length, 1);
+
+      searcher = MemoStoreSearcher(memoStore, 'TEST');
+      searcher.execute();
+      expect(searcher.results.length, 1);
+
+      searcher = MemoStoreSearcher(memoStore, 'テスト');
+      searcher.execute();
+      expect(searcher.results.length, 1);
+    });
+    
     test('MemoStoreSearcher should parse a query as keywords.', () {
       final memoStore = MemoStore();
       final memo = Memo();
