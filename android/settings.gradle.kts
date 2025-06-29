ce23a2ddb8527cb061b37dd790a2f2389f6cb70e
Yasuaki Gohko <ygohko@gmail.com>
2025-06-29 16:22:59 +0900

    Cleaned up.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -23,37 +23,3 @@ plugins {
 }
 
 include(":app")
-
-/*
-import java.util.Properties
-import java.io.File
-
-// ':app' モジュールをインクルードします。
-include(":app")
-
-// local.properties ファイルのパスを解決します。
-// `rootDir` はルートプロジェクトのディレクトリを指すプロパティです。
-val localPropertiesFile = File(rootDir, "local.properties")
-
-// ファイルが存在しない場合はビルドを失敗させます。
-require(localPropertiesFile.exists()) {
-    "local.properties file not found in ${rootDir.path}"
-}
-
-// Propertiesオブジェクトを作成し、ファイルを読み込みます。
-val properties = Properties()
-localPropertiesFile.reader(Charsets.UTF_8).use { reader ->
-    properties.load(reader)
-}
-
-// local.propertiesからFlutter SDKのパスを取得します。
-val flutterSdkPath = properties.getProperty("flutter.sdk")
-
-// flutter.sdkプロパティが設定されていない場合はビルドを失敗させます。
-requireNotNull(flutterSdkPath) {
-    "flutter.sdk not set in local.properties"
-}
-
-// Flutterのプラグインローダースクリプトを適用します。
-apply(from = "$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle")
-*/
