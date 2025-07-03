c01d046a1acc58397c6a020222a91dd7935279f7
Yasuaki Gohko <ygohko@gmail.com>
2025-07-03 16:54:38 +0900

    Added a test for getSynchronizingHidden().

--- test/setting_test.dart
+++ test/setting_test.dart
@@ -3,6 +3,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tsukimisou/settings.dart';
 
 class MockSharedPreferencesWithCache implements SharedPreferencesWithCache {
+  bool? getBool(String key) {
+    return true;
+  }
+
   noSuchMethod(Invocation invocation) {
     assert(false);
   }
@@ -15,6 +19,17 @@ void main() {
     test('Settings should be created', () {
       expect(Settings(), isNotNull);
     });
+
+    test('Settings.getSynchronizing() should whether synchronizing is hidden', () async {
+        Settings.sharedPreferencesCreatorHook = () async {
+          return MockSharedPreferencesWithCache();
+        };
+
+        final settings = Settings();
+        final hidden = await settings.getSynchronizingHidden();
+        expect(hidden, true);
+    });
+
 
     // TODO: Add tests for getters and setters.
   });
