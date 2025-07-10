e1de35c36a8763e7da22f624775a78931a1d38e0
Yasuaki Gohko <ygohko@gmail.com>
2025-07-11 07:58:15 +0900

    Adjusting tests.

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,17 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tsukimisou/app.dart';
 import 'package:tsukimisou/app_state.dart';
 import 'package:tsukimisou/memo_store.dart';
+import 'package:tsukimisou/settings.dart';
+
+// ignore: must_be_immutable
+class MockSharedPreferencesWithCache implements SharedPreferencesWithCache {
+  var _hidden = false;
+  var _tagScores = '{"a": 1.0, "b": 0.5}';
+
+  @override
+  bool? getBool(String key) {
+    return _hidden;
+  }
+
+  @override
+  String? getString(String key) {
+    return _tagScores;
+  }
+
+  @override
+  Future<void> setBool(String key, bool value) async {
+    _hidden = value;
+  }
+
+  @override
+  Future<void> setString(String key, String value) async {
+    _tagScores = value;
+  }
+
+  @override
+  noSuchMethod(Invocation invocation) {
+    throw UnimplementedError();
+  }
+}
 
 void main() {
+  Settings.sharedPreferencesCreatorHook = () async {
+    return MockSharedPreferencesWithCache();
+  };
+
   testWidgets('App widget smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(
       MultiProvider(
         providers: [
           ChangeNotifierProvider<MemoStore>(create: (context) => MemoStore()),
           ChangeNotifierProvider<AppState>(create: (context) => AppState()),
+          ChangeNotifierProvider<Settings>(create: (context) => Settings()),
         ],
         child: const App(),
       ),
@@ -21,4 +59,6 @@ void main() {
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
   });
+
+  Settings.sharedPreferencesCreatorHook = null;
 }
--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tsukimisou/app_state.dart';
 import 'package:tsukimisou/home_page.dart';
 import 'package:tsukimisou/memo_store.dart';
@@ -8,7 +9,39 @@ import 'package:tsukimisou/memo_store_google_drive_loader.dart';
 import 'package:tsukimisou/memo_store_google_drive_saver.dart';
 import 'package:tsukimisou/memo_store_local_loader.dart';
 import 'package:tsukimisou/memo_store_local_saver.dart';
+import 'package:tsukimisou/settings.dart';
 import 'package:tsukimisou/gen_l10n/app_localizations.dart';
+
+// ignore: must_be_immutable
+class MockSharedPreferencesWithCache implements SharedPreferencesWithCache {
+  var _hidden = false;
+  var _tagScores = '{"a": 1.0, "b": 0.5}';
+
+  @override
+  bool? getBool(String key) {
+    return _hidden;
+  }
+
+  @override
+  String? getString(String key) {
+    return _tagScores;
+  }
+
+  @override
+  Future<void> setBool(String key, bool value) async {
+    _hidden = value;
+  }
+
+  @override
+  Future<void> setString(String key, String value) async {
+    _tagScores = value;
+  }
+
+  @override
+  noSuchMethod(Invocation invocation) {
+    throw UnimplementedError();
+  }
+}
 
 Future<void> init(WidgetTester tester) async {
   await tester.pumpWidget(
@@ -16,6 +49,7 @@ Future<void> init(WidgetTester tester) async {
       providers: [
         ChangeNotifierProvider<MemoStore>(create: (context) => MemoStore()),
         ChangeNotifierProvider<AppState>(create: (context) => AppState()),
+        ChangeNotifierProvider<Settings>(create: (context) => Settings()),
       ],
       child: const MaterialApp(
         localizationsDelegates: [
@@ -40,6 +74,9 @@ void main() {
   MemoStoreGoogleDriveSaver.constructorHook = (memoStore, fileName) {
     return MemoStoreMockGoogleDriveSaver(memoStore, fileName);
   };
+  Settings.sharedPreferencesCreatorHook = () async {
+    return MockSharedPreferencesWithCache();
+  };
 
   group('HomePage', () {
     testWidgets('HomePage shoud have specified widgets.',
@@ -60,6 +97,7 @@ void main() {
     });
   });
 
+  Settings.sharedPreferencesCreatorHook = null;
   MemoStoreGoogleDriveSaver.constructorHook = null;
   MemoStoreGoogleDriveLoader.constructorHook = null;
   MemoStoreLocalSaver.constructorHook = null;
