58ac83604e4991768dc62b86d291ca8b4c9dc8a8
Yasuaki Gohko <ygohko@gmail.com>
2025-08-05 19:46:11 +0900

    Update colors for code spans and blocks (#408)
    * Updated colors for codes.
    
    * Adjust background color.
    
    * Updated background color.
    
    * Adjusted code colors.

--- lib/common_uis.dart
+++ lib/common_uis.dart
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Yasuaki Gohko
+ * Copyright (c) 2022 - 2025 Yasuaki Gohko
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -63,8 +63,11 @@ class TsukimisouColors {
   /// Color for checked check box.
   static const checkBoxChecked = Colors.green;
 
+  /// Color for code text.
+  static final codeText= Color(0xFFFFFFFF);
+
   /// Color for code background.
-  static final codeBackground = Colors.grey[200];
+  static final codeBackground = Color(0x9F00000F);
 
   /// Color for block quote indicator.
   static const blockQuoteIndicator = Colors.grey;
@@ -113,6 +116,7 @@ class TsukimisouTextStyles {
     style ??= const TextStyle();
     style = GoogleFonts.mPlus1Code(textStyle: style);
     style = style.apply(
+      color: TsukimisouColors.codeText,
       backgroundColor: TsukimisouColors.codeBackground,
       fontWeightDelta: 2,
     );
