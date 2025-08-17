87f3a0b79ed659454ccec1eeaf31fca084eb2cfc
Yasuaki Gohko <ygohko@gmail.com>
2025-08-18 07:40:33 +0900

    Added test codes to colorize code block.

--- lib/markdown_parser.dart
+++ lib/markdown_parser.dart
@@ -284,12 +284,23 @@ class MarkdownParser {
             break;
 
           case _LineKind.code:
+            widget = Container(
+              color: Colors.blue,
+              child: Text.rich(
+                TextSpan(
+                  style: TsukimisouTextStyles.viewingPageCode(_context),
+                  children: processedLine.spans,
+                ),
+              ),
+            );
+            /*
             widget = Text.rich(
               TextSpan(
                 style: TsukimisouTextStyles.viewingPageCode(_context),
                 children: processedLine.spans,
               ),
             );
+            */
             break;
 
           case _LineKind.blockQuote:
