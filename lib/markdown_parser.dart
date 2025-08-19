62f72576a52fce04c54f3a82782df6d3ce1bc749
Yasuaki Gohko <ygohko@gmail.com>
2025-08-20 07:37:46 +0900

    Update background color for code blocks (#412)
    * Added test codes to adjust code block background.
    
    * Updated constants for code backgrounds.

--- lib/markdown_parser.dart
+++ lib/markdown_parser.dart
@@ -288,7 +288,7 @@ class MarkdownParser {
               children: [
                 Expanded(
                   child: Container(
-                    color: TsukimisouColors.codeBackground,
+                    color: TsukimisouColors.codeBlockBackground,
                     child: Text.rich(
                       TextSpan(
                         style: TsukimisouTextStyles.viewingPageCodeBlock(_context),
