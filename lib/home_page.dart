6ddb279fda99de3cc4ff2068310cab1d72c5147f
Yasuaki Gohko <ygohko@gmail.com>
2026-06-22 19:05:58 +0900

    Fix issue where main MemoStore is saved instead of archive MemoStores (#455)
    * Added a TODO.
    
    * Fixed issue where main MemoStore is saved instead of archive MemoStores.
    
    * Removed a TODO.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -313,7 +313,8 @@ class _HomePageState extends State<HomePage> {
 
     final updatedArchiveNames = merger.updatedArchiveNames;
     for (final name in updatedArchiveNames) {
-      final saver = await MemoStoreLocalSaver.fromFileName(toMemoStore, 'Archive-$name.json');
+      final memoStore = await toMemoStore.archiveMemoStore(name);
+      final saver = await MemoStoreLocalSaver.fromFileName(memoStore, 'Archive-$name.json');
       try {
         await saver.execute();
       } on Exception catch (exception, stackTrace) {
