c614aa561d63242ef961471089825907e6ad6a88
Yasuaki Gohko <ygohko@gmail.com>
2026-06-23 18:39:40 +0900

    Cleaned up.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -312,15 +312,8 @@ class _HomePageState extends State<HomePage> {
     }
 
     final updatedArchiveNames = merger.updatedArchiveNames;
-
-    // TODO: Upload archive MemoStores if it does not exist in Google Drive.
-    print('updatedArchiveNames: $updatedArchiveNames');
-
     for (final name in updatedArchiveNames) {
       final memoStore = await toMemoStore.archiveMemoStore(name);
-
-      print('memoStore.memos.length: ${memoStore.memos.length}');
-
       final saver = await MemoStoreLocalSaver.fromFileName(memoStore, 'Archive-$name.json');
       try {
         await saver.execute();
@@ -364,9 +357,6 @@ class _HomePageState extends State<HomePage> {
 
     for (final name in updatedArchiveNames) {
       final memoStore = await toMemoStore.archiveMemoStore(name);
-
-      print('memoStore.memos.length: ${memoStore.memos.length}');
-
       final saver = MemoStoreGoogleDriveSaver(memoStore, 'Archive-$name.json');
       try {
         await saver.execute();
