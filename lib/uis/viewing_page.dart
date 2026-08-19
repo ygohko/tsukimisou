38dd937bef550a2f75356465cd7d2b2e427b475a
Yasuaki Gohko <ygohko@gmail.com>
2026-08-20 07:31:26 +0900

    Added test codes.

--- lib/uis/viewing_page.dart
+++ lib/uis/viewing_page.dart
@@ -603,7 +603,7 @@ class _ViewingPageState extends State<ViewingPage>
             mainAxisSize: MainAxisSize.min,
             children: [
               Spacer(
-                flex: upperSpacerFlex,
+                flex: 1, // upperSpacerFlex,
               ),
               SizedBox(
                 height: 400.0,
@@ -654,7 +654,7 @@ class _ViewingPageState extends State<ViewingPage>
                 ]),
               ),
               Spacer(
-                flex: lowerSpacerFlex,
+                flex: 1, // lowerSpacerFlex,
               ),
             ],
           );
